4a9585f16e8bf0841846d8be4f050579f8454291
Sheshank Suresh <sheshank.suresh@yahoo.com>
2022-12-31 20:40:58 -0500

    modified api folder structure

--- api/app.py
+++ api/app.py
@@ -1,33 +1,9 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
+from flask import Flask, Blueprint
+
+from test_api import test_bp
 
 app = Flask(__name__)
-api = Api(app)
 
-CREATORS = ["Sheshank", "Shubh", "Kriya", "Richard"]
-
-class TestClass(Resource):
-    # Testing methods for api
-    def __init__(self) -> None:
-        self.creators = CREATORS
-        super().__init__()
-
-    @app.route("/")
-    def test_entry():
-        return "Hello Dr. Pill!"
-    
-    @app.route("/creators", methods=['GET'])
-    def test_creators(self):
-        print(f"My creators are: {creator}" for creator in self.creators)
-        return 
-    
-test = TestClass()
-api.add_resource(test, '/test') # /test is the entry point to test basic API functionality
-
-@app.route("/home/")
-def test_hello():
-    return "Hello!"
-
-if __name__ == '__main__':
-    app.run()
-
+if __name__ == "__main__":
+    app.register_blueprint(test_bp, url_prefix='/api/v1/test')
+    app.run(debug=True)
