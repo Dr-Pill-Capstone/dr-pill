67d633b65de29dbdb8bce817fac7f24bc29b4473
Sheshank Suresh <sheshanknair@gmail.com>
2023-03-26 21:06:30 -0400

    added motor api and test motor file

--- api/motor.py
+++ api/motor.py
@@ -4,7 +4,7 @@ import time
 class ServoMotor:
     def __init__(self, pin):
         self.pin = pin
-        GPIO.setmode(GPIO.BCM)
+        GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.pin, GPIO.OUT)
         self.pwm = GPIO.PWM(self.pin, 50) # 50 Hz PWM frequency
         self.pwm.start(0)
@@ -19,34 +19,6 @@ class ServoMotor:
     
     def set_speed(self, speed):
         self.pwm.ChangeFrequency(speed) # change PWM frequency to adjust speed
-    
-    def set_limits(self, min_angle, max_angle):
-        self.min_angle = min_angle
-        self.max_angle = max_angle
-    
-    def set_range(self, min_position, max_position):
-        self.min_position = min_position
-        self.max_position = max_position
-    
-    def set_relative_angle(self, angle):
-        current_duty_cycle = self.pwm.get_duty_cycle()
-        current_angle = (current_duty_cycle - 2.5) * 18.0
-        new_angle = current_angle + angle
-        if new_angle < self.min_angle:
-            new_angle = self.min_angle
-        elif new_angle > self.max_angle:
-            new_angle = self.max_angle
-        self.set_angle(new_angle)
-    
-    def set_relative_position(self, position):
-        current_duty_cycle = self.pwm.get_duty_cycle()
-        current_position = (current_duty_cycle - 2.5) / 10.0 * 100.0
-        new_position = current_position + position
-        if new_position < self.min_position:
-            new_position = self.min_position
-        elif new_position > self.max_position:
-            new_position = self.max_position
-        self.set_position(new_position)
     
     def cleanup(self):
         self.pwm.stop()
